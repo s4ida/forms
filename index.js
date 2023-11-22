a2af22f6554ada3cf75459e8ea162c9947375067
s4ida <sd88c9gvh@code.edu.az>
2023-11-22 16:57:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const submit=document.getElementById('myform')
+const nameinput = document.getElementById('nameinp');
+const surnameinput = document.getElementById('surnameinp');
+const ageinput = document.getElementById('ageinp');
+const emailinput = document.getElementById('emailinp');
+const usernameinput=document.getElementById('usernameinp');
+const passwordinput=document.getElementById('passwordinp');
+const addressinput=document.getElementById('addressinp');
+const countryinput=document.getElementById('countryinp')
+const titleinput=document.getElementById('titleinp');
+const descriptioninput=document.getElementById('descriptioninp');
+const discountinput=document.getElementById('discountinp');
+const stockinput=document.getElementById('stockinp');
+const qualityinput=document.getElementById('qualityinp');
+const customerscountinput=document.getElementById('countinp');
+const categoryinput=document.getElementById('categoryinp');
+const cityinput=document.getElementById('cityinp')
+const priceinput=document.getElementById('priceinp')
+
+
+
+// 1---NAME,SURNAME,AGE,EMAIL
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         name: nameinput.value,
+//         surname: surnameinput.value,
+//         age: ageinput.value,
+//         email: emailinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
+
+
+// 2---USERNAME,EMAIL,PASSWORD,ADDRES,COUNTRY
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         username: usernameinput.value,
+//        email: emailinput.value,
+//         password: passwordinput.value,
+//         address: addressinput.value,
+//        country: countryinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
+
+
+
+// 3---TITLE,PRICE,DESCRIPTION,DISCOUNT,STOCK,QUALITY,COUNTRY,СUSTOMERSCOUNT,CATEGORY,CITY 
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         title: titleinput.value,
+//        price: priceinput.value,
+//         description: descriptioninput.value,
+//        discount: discountinput.value,
+//        stock: stockinput.value,
+//        quality :qualityinput.value,
+//        country:countryinput.value,
+//        customerscount:customerscountinput.value,
+//        category:categoryinput.value,
+//        city:cityinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
